55c423ba55557aab6f0a435bdc3a81b2c8f7e756
nemutas <inohiro.focru@gmail.com>
2024-08-25 00:59:56 +0900

    init

--- src/scripts/entry.ts
+++ src/scripts/entry.ts
@@ -1,6 +1,7 @@
 import { Canvas } from './Canvas'
 
-console.log(`https://github.com/nemutas/${import.meta.env.BASE_URL.split('/').at(-2)}`)
+console.log(`github: https://github.com/nemutas/${import.meta.env.BASE_URL.split('/').at(-2)}`)
+console.log('others: https://inoue-prototype-work.vercel.app/')
 
 const canvas = new Canvas(document.querySelector<HTMLCanvasElement>('.webgl-canvas')!)
 
